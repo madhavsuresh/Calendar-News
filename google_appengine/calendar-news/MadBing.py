467f155d9f2e7094be8c214c1a56560be06b78e2
Madhav Suresh <madman92@gmail.com>
2011-11-09 09:37:55 -0600

    Added wolframalpha api

--- google_appengine/calendar-news/MadBing.py
+++ google_appengine/calendar-news/MadBing.py
@@ -2,33 +2,24 @@
 # Madhav Suresh 2011
 
 import urllib
-
-
-class PackedUrl():
-  ''' Request object that is sent to Alchemy API, not all calls
-  are supported right now, only the subset we need '''
-  _api_key = ''
-  _query = ''
-  _sources = ''
-  
-  def __init__(self,api,txt,sources):
-    
-    self._api_key = api
-    self._query = urllib.quote_plus(txt)
-    self._sources = sources
-
-  def getUrl(self,endpoint):
-    endpoint = endpoint + '?Appid=' + self._api_key + '&query=' + self._query + '&sources=' + self._sources + '&' + self._sources+ '.count=3'
-    return endpoint
+from xml.dom import minidom
 
 class Bing():
+  ''' Request object that is sent to Bing API, not all calls
+  are supported right now, only the subset we need '''
   _api_key = ''
   _endpoint = 'http://api.search.live.net/json.aspx'
 
 
-
   def __init__(self,api_path):
     self.readAPIKey(api_path)
+
+  def makeResponse(self,txtReq,reqType,count):
+    return self._endpoint + \
+	   '?Appid=' + self._api_key + \
+	   '&query=' + txtReq + \
+	   '&sources=' + reqType + \
+	   '&' + reqType + '.count=' + str(count)
 
   def readAPIKey(self,fileName):
     f = open(fileName,'r')
@@ -37,17 +28,45 @@ class Bing():
     self._api_key = x['bing']
     print self._api_key
 
-  def getAPIKey(self):
-    return self._api_key
-
-  def getNewsResults(self,txt_req):
-    p = PackedUrl(self._api_key,txt_req,'news')
-    result = urllib.urlopen(p.getUrl(self._endpoint)).read()
+  def getNewsResults(self,txtReq,count):
+    p = self.makeResponse(txtReq,'news',count)
+    result = urllib.urlopen(p).read()
     return result
 
-  def getWebResults(self,txt_req):
-    p = PackedUrl(self._api_key,txt_req,'web')
-    result = urllib.urlopen(p.getUrl(self._endpoint)).read()
+  def getWebResults(self,txtReq,count):
+    p = self.makeResponse(txtReq,'web',count)
+    result = urllib.urlopen(p).read()
     return result
 
-    
+class WolframAlpha():
+  ''' Request object that is sent to Bing API, not all calls
+  are supported right now, only the subset we need '''
+  _api_key = ''
+  _endpoint = 'http://api.wolframalpha.com/v2/query?'
+#
+
+
+  def __init__(self,api_path):
+    self.readAPIKey(api_path)
+
+  def makeResponse(self,txtReq,reqType):
+    return self._endpoint + \
+	   'appid=' + self._api_key + \
+	   '&input=' + txtReq + \
+	   '&format=' + reqType + \
+           '&includepodid=DateRangeSpecified:Close:FinancialData'
+
+  def readAPIKey(self,fileName):
+    f = open(fileName,'r')
+    line = f.read().strip()
+    x = dict(kvp.split('=') for kvp in line.split(';'))
+    self._api_key = x['wolframalpha']
+    print self._api_key
+
+  def getStockResult(self,txtReq):
+    print self._api_key
+    p = self.makeResponse(txtReq + 'stock last week','image,plaintext')
+    result = urllib.urlopen(p).read()
+    dom = minidom.parseString(result)
+    x = dom.getElementsByTagName('img')[0]
+    return x.getAttribute('src')
---
+++ google_appengine/calendar-news/test.py
@@ -0,0 +1,32 @@
+from MadBing import WolframAlpha,Bing
+import wap
+from MadAlchemy import PackedRequest,Alchemy
+import simplejson as json
+from collections import defaultdict
+
+def multidict(ordered_pairs):
+    """Convert duplicate keys values to lists."""
+    # read all values into lists
+    d = defaultdict(list)
+    for k, v in ordered_pairs:
+        d[k].append(v)
+
+    # unpack lists that have only 1 item
+    for k, v in d.items():
+        if len(v) == 1:
+            d[k] = v[0]
+    return dict(d)
+
+
+b = Alchemy('api_keys.txt')
+a = Bing('api_keys.txt')
+
+w = WolframAlpha('api_keys.txt')
+x = w.getStockResult("google")
+print x
+#txt = b.getRankedTxtEntities('Proctor and Gamble')
+
+server = 'http://api.wolframalpha.com/v2/query?'
+appid = '5TV6XG-297R536AJ4'
+input = 'who are you?'
+
